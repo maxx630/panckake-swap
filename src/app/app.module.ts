9be80c670ed99537db39697319037342f21339d8
casamentoignazio <casamento.ignazio@gmail.com>
2022-02-08 17:41:51 +0100

    add components and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ConnectWalletComponent } from './shared/connect-wallet/connect-wallet.component';
+import { HeroComponent } from './features/home/hero/hero.component';
+import { EarnPassiveIncomeComponent } from './features/home/earn-passive-income/earn-passive-income.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ConnectWalletComponent,
+    HeroComponent,
+    EarnPassiveIncomeComponent
   ],
   imports: [
     BrowserModule
