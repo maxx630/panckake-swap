1fae31891ae82e46ad36c74ce751025590c7473f
casamentoignazio <casamento.ignazio@gmail.com>
2022-02-11 21:14:54 +0100

    fix core folder add font add earn passive income section

--- src/app/features/home/earn-passive-income/earn-passive-income.component.ts
+++ src/app/features/home/earn-passive-income/earn-passive-income.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./earn-passive-income.component.css']
 })
 export class EarnPassiveIncomeComponent implements OnInit {
-
+  
+  public myInterval = setInterval(this.changeTitle, 3000);
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  private changeTitle() : void {
+    const firstEl = document.getElementById("firstTitle");
+    const secondEl = document.getElementById("secondTitle");
+    const thirdEl = document.getElementById("secondSyrup");
+    const fourthEl = document.getElementById("secondFarm");
+    firstEl!.style.display = firstEl!.style.display == "flex" ? "none" : "flex";
+    secondEl!.style.display = secondEl!.style.display == "none" ? "flex" : "none";
+    thirdEl!.style.display = thirdEl!.style.display == "flex" ? "none" : "flex";
+    fourthEl!.style.display = fourthEl!.style.display == "none" ? "flex" : "none";
+  }
+
 }
+
+ 
+
