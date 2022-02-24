53ef5657db96954a462a303bf12ccf0cdaa89c6f
casamentoignazio <casamento.ignazio@gmail.com>
2022-02-24 08:54:47 +0100

    add overview page

---
+++ src/app/features/nfts/collections/collections.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'psclone-collections',
+  templateUrl: './collections.component.html',
+  styleUrls: ['./collections.component.css']
+})
+export class CollectionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/features/nfts/overview/faqs/faqs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'psclone-faqs',
+  templateUrl: './faqs.component.html',
+  styleUrls: ['./faqs.component.css']
+})
+export class FaqsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
