47a1473fc8c42fd9d56e35e2971955299ab40efb
Valentina <lilithsixx@gmail.com>
2022-03-04 16:04:13 +0100

    fix: changeVisual method

--- src/app/shared/swap/swap.component.ts
+++ src/app/shared/swap/swap.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwapComponent implements OnInit {
   active = 1;
+  element1!: string;
+  element2!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.alternateBorder();
   }
 
+  alternateBorder() {
+    const element1 = document.getElementById("pills-exc-tab");
+    const element2 = document.getElementById("pills-liq-tab");
+
+    element2!.addEventListener('click', function () {
+      element2!.classList.add('green-b', 'secondary-v');
+      element1!.classList.remove("green-b", 'secondary-v');
+    })
+
+    element1!.addEventListener('click', function () {
+      element1!.classList.add('green-b', 'secondary-v');
+      element2!.classList.remove("green-b", 'secondary-v');
+    })
+  }
 }
