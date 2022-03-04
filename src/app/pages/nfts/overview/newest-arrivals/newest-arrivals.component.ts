5661af02f3d84efe7019857980f0901f1c4ee0a9
casamentoignazio <casamento.ignazio@gmail.com>
2022-03-04 19:12:11 +0100

    add nfts services

--- src/app/pages/nfts/overview/newest-arrivals/newest-arrivals.component.ts
+++ src/app/pages/nfts/overview/newest-arrivals/newest-arrivals.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-interface ArrivalCard {
-  img: string,
-  description: string,
-  name: string,
-  price: number
-}
+import { NewestArrivalsService } from 'src/app/core/service/newest-arrivals.service';
+import { IArrivalCard } from 'src/app/models/arrivalCards';
 
 @Component({
   selector: 'psclone-newest-arrivals',
@@ -14,28 +9,12 @@ interface ArrivalCard {
 })
 export class NewestArrivalsComponent implements OnInit {
 
-  obj: ArrivalCard[] = [
-    {img: '../../../../../assets/overview/backg.png', description: 'Lil Bulls', name: 'Lil Bulls #3016', price: 1847.39},
-    {img: '../../../../../assets/overview/backg2.png', description: 'Pancake Squad', name: 'Pancake Squad #710', price: 369.48},
-    {img: '../../../../../assets/overview/backg3.png', description: 'Gamester Apes', name: 'Gamester #3463', price: 7031},
-    {img: '../../../../../assets/overview/backg4.png', description: 'BornBadGirls', name: 'Girl #3009', price: 110.84},
-    {img: '../../../../../assets/overview/backg5.png', description: 'ShitPunks', name: 'Shit Punk 9889', price: 121.93},
-    {img: '../../../../../assets/overview/backg6.png', description: 'Saint Monster', name: 'Observation_165', price: 144.64},
-    {img: '../../../../../assets/overview/backg7.png', description: 'Lil Bulls', name: 'Lil Bulls #1636', price: 81.28},
-    {img: '../../../../../assets/overview/backg8.png', description: 'Pancake Squad', name: 'Pancake Squad #462', price: 1588.40},
-    {img: '../../../../../assets/overview/backg9.png', description: 'Lil Bulls', name: 'Lil Bulls #3346', price: 277.05},
-    {img: '../../../../../assets/overview/backg10.png', description: 'Lil Bulls', name: 'Lil Bulls #1945', price: 147.76},
-    {img: '../../../../../assets/overview/backg11.png', description: 'ShitPunks', name: 'Shit Punk 2333', price: 55.41},
-    {img: '../../../../../assets/overview/backg12.png', description: 'BornBadBoys', name: 'BornBadBoys #3977', price: 73.85},
-    {img: '../../../../../assets/overview/backg13.png', description: 'BornBadBoys', name: 'BornBadBoys #4387', price: 73.86},
-    {img: '../../../../../assets/overview/backg14.png', description: 'The Bull Society', name: '#5160', price: 111.19},
-    {img: '../../../../../assets/overview/backg15.png', description: 'Pancake Bunnies', name: 'Stormy Easter', price: 18.91},
-    {img: '../../../../../assets/overview/backg16.png', description: 'Pancake Bunnies', name: 'Sleepy', price: 6.60}
-  ]
+  card: IArrivalCard[] = [];
 
-  constructor() { }
+  constructor(private cards: NewestArrivalsService) { }
 
   ngOnInit(): void {
+    this.card = this.cards.getAll();
   }
 
 }
--- src/app/pages/nfts/overview/newest-collections/newest-collections.component.ts
+++ src/app/pages/nfts/overview/newest-collections/newest-collections.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
-export interface Cards {
-  img: string,
-  avatar: string,
-  name: string,
-  value: number
-}
+import { NewestCollectionsService } from 'src/app/core/service/newestCollections.service';
+import { INftsCards } from 'src/app/models/nftsCards';
 
 @Component({
   selector: 'psclone-newest-collections',
@@ -14,18 +9,12 @@ export interface Cards {
 })
 export class NewestCollectionsComponent implements OnInit {
 
-  card: Cards[] = [
-    { img: '../../../../../assets/overview/banner-sm.png', avatar: '../../../../../assets/overview/avatar.png', name: 'Liquid Monster', value: 52.79429 },
-    { img: '../../../../../assets/overview/banner-sm1.png', avatar: '../../../../../assets/overview/avatar1.png', name: 'WolfiLeague', value: 16.4189 },
-    { img: '../../../../../assets/overview/banner-sm2.png', avatar: '../../../../../assets/overview/avatar2.png', name: 'Lil Bulls', value: 82.28029 },
-    { img: '../../../../../assets/overview/banner-sm3.png', avatar: '../../../../../assets/overview/avatar3.png', name: 'Kittens Meow Meow', value: 55.98296 },
-    { img: '../../../../../assets/overview/banner-sm4.png', avatar: '../../../../../assets/overview/avatar4.png', name: 'Cross Boses', value: 23.95 },
-    { img: '../../../../../assets/overview/banner-sm5.png', avatar: '../../../../../assets/overview/avatar5.png', name: 'Saint Monster', value: 61.9298 }
-  ]
+  cards: INftsCards[] = [];
 
-  constructor() { }
+  constructor(private nftsCards: NewestCollectionsService) { }
 
   ngOnInit(): void {
+    this.cards = this.nftsCards.getAll();
   }
 
 }
